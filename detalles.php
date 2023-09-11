5dacfe4a6bc495caf7a1fe6601eed00747d6ccdc
Oscar Arias <oscar.arias@integralservicios.com.co>
2023-09-11 17:15:19 -0500

    links
    links

--- detalles.php
+++ detalles.php
@@ -28,10 +28,12 @@
         <h2>El señor de las PELICULAS</h2>
     </div>
     <ul class="menu">
-        <li><a href="Admin.php">Inicio</a></li>
-        <li><a href="catalogo_peliculas.php">Ver peliculas</a></li>
-        <li><a href="series.php">Ver series</a></li>
-        <li><a href="login.php">Login</a></li>
+    <li><a href="index.php">Inicio</a></li>
+<li><a href="crear.php">Crear</a></li>
+<li><a href="eliminar.php">Eliminar</a></li>
+<li><a href="ver.php">Ver</a></li>
+<li><a href="login.php">Login</a></li>
+<li><a href="index.php">Salir</a></li>>
     </ul>
   
     
